3cdbdeebecf823319dea430775153f092d4725ea
multani <multani@yahoo-inc.com>
2014-10-13 22:11:45 +0800

    init quick sort

---
+++ quickSort.hs
@@ -0,0 +1,16 @@
+-- quickSort :: (Ord a) => [a] -> [a]
+
+
+quickSort [] = []
+quickSort x = case x of
+                x:xs -> case xs of 
+                  y:ys -> (quickSort (getSmaller x xs)) ++ [x] ++ (quickSort (getLarger x xs))
+                  [] -> [x]
+
+getSmaller :: (Ord a) => a -> [a] -> [a]
+getSmaller x [] = []
+getSmaller x xs = foldr (\y acc -> if (y <= x) then y:acc else acc) [] xs
+
+getLarger :: (Ord a) => a -> [a] -> [a]
+getLarger x [] = []
+getLarger x xs = foldr (\y acc -> if (y > x) then y:acc else acc) [] xs
